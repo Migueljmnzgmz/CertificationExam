eeff017ba0ab6f096584187601ceedd6a5b668d2
Miguel <mj601586@gmail.com>
2026-04-30 08:18:02 -0600

    Dia 0 Hackerrank

---
+++ src/main/java/com/ux/certification/treintaDiasXCodes/Day0Solution.java
@@ -0,0 +1,18 @@
+package com.ux.certification.treintaDiasXCodes;
+
+import java.util.Scanner;
+
+public class Day0Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String inputString = scanner.nextLine();
+
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/com/ux/certification/treintaDiasXCodes/Main.java
@@ -0,0 +1,10 @@
+package com.ux.certification.treintaDiasXCodes;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Day0Solution solution = new Day0Solution();
+        solution.saludar();
+
+    }
+}
