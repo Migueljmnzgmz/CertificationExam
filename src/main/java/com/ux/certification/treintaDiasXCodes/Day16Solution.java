7d856f890d494a4462909734d1c0e59941ad3095
Miguel <mj601586@gmail.com>
2026-05-15 16:55:50 -0600

    Hackerrank

---
+++ src/main/java/com/ux/certification/treintaDiasXCodes/Day16Solution.java
@@ -0,0 +1,17 @@
+package com.ux.certification.treintaDiasXCodes;
+
+import java.util.Scanner;
+
+public class Day16Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String string = in.next();
+        in.close();
+        try {
+            Integer integer = Integer.parseInt(string);
+            System.out.println(integer);
+        } catch (NumberFormatException numberFormatException) {
+            System.out.println("Bad String");
+        }
+    }
+}
---
+++ src/main/java/com/ux/certification/treintaDiasXCodes/Day5Solution.java
@@ -0,0 +1,15 @@
+package com.ux.certification.treintaDiasXCodes;
+
+import java.util.Scanner;
+
+public class Day5Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        in.close();
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+    }
+}
