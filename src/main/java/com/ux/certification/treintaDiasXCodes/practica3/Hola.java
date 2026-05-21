d614334bbfd4e1d542d44d6047062a208a0aa706
Miguel <mj601586@gmail.com>
2026-05-21 08:27:56 -0600

    Commjt simulacion examen

---
+++ src/main/java/com/ux/certification/treintaDiasXCodes/practica3/Hola.java
@@ -0,0 +1,8 @@
+package com.ux.certification.treintaDiasXCodes.practica3;
+
+public class Hola{
+        public static void main(String[] args) {
+            System.out.println("Hola Mundo");
+        }
+    }
+
