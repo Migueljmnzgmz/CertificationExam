6bd3c37e58f1e4a50ba85ac02e773071a1a7aaed
Miguel <mj601586@gmail.com>
2026-05-22 07:56:24 -0600

    Commit simulacion examen

---
+++ src/main/java/ejerciciosSimulacion/Simulacion1.java
@@ -0,0 +1,28 @@
+package ejerciciosSimulacion;
+import java.util.ArrayList;
+
+public class Simulacion1 {
+
+/*
+        ArrayList<Integer> scores;
+        private double average;
+
+        public ArrayList<Integer> getScores(){ return scores; }
+
+        public double getAverage(){ return average; }
+
+        private void computeAverage(){
+            //valid code to compute average
+            average =//update average value
+        }
+
+        public Simulacion1(){
+            computeAverage();
+        }
+
+Codio dado por el examen
+
+la encapsulacion seria mejor porque protege las variables de ser modificadas desde afuera y conseguir un getcore para imprimir la copia de
+la lista
+
+*/}
